80e03920d34f6582933da81e4f590f520a04fb20
Andrew <paul.rueda@hotmail.com>
2025-02-08 16:53:47 -0500

    security helmet limiter and sanitizer for nosql injections

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-    MONGO_URI: process.env.MONGO_URI || false
+    MONGO_URI: process.env.MONGO_URI || false,
+    PORT: process.env.PORT || false
 };
