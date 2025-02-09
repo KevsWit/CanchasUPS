8f520dda22a2416c1ceefb1eb803bd36d9b484b4
Kevin Castillo <96481791+KevsWit@users.noreply.github.com>
2025-02-09 13:12:49 -0500

    Actualización rutas
    Actualización rutas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,16 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { AcercaDeComponent } from './pages/acerca-de/acerca-de.component';
 import { ReservaComponent } from './pages/reserva/reserva.component';
+import { PrivacyComponent } from './pages/privacy/privacy.component';
+import { TermsComponent } from './pages/terms/terms.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/inicio', pathMatch: 'full' },
     { path: 'inicio', component: InicioComponent },
     { path: 'acerca-de', component: AcercaDeComponent },
-    { path: 'reserva', component: ReservaComponent }
+    { path: 'reserva', component: ReservaComponent },
+    { path: 'privacy', component: PrivacyComponent },
+    { path: 'terms', component: TermsComponent },
+    { path: 'contact', component: ContactComponent }
 ];
