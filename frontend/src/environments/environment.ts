9b3c750db733ea1d2618fb75356e260edbebd2b0
Kevin Castillo <96481791+KevsWit@users.noreply.github.com>
2025-02-09 13:07:55 -0500

    Configuracion para conexion front-back
    Configuracion para conexion front-back

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://4df9-200-63-104-90.ngrok-free.app/api' // URL pública en producción
+    apiUrl: 'https://####-####.ngrok-free.app/api' // URL pública en producción
   };
   
